5c1f475d1e3ef7f43e5d4afb2a95709ff814d316
Rremesal <34509994+Rremesal@users.noreply.github.com>
2022-11-30 12:14:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# myblog
+ A blog we need to make for a school assignment
---
+++ app/Models/comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class comment extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    public function post() {
+        return $this->belongsTo(post::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(user::class);
+    }
+}
